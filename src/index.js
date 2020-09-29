79279946da4148cfdc1a592f6a14cccaa9030b97
Irina Selivanova <irina.selivanova1106@gmail.com>
2020-09-29 13:33:31 +0300

    feat: add min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 
 exports.min = function min (array) {
-  return 0;
+    let arraySorted;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    arraySorted = quickSort(array);
+    return arraySorted[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+    let arraySorted;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+    arraySorted = quickSort(array);
+    return arraySorted[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    let arraySum = 0;
+
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        arraySum += array[i];
+    }
+
+    return arraySum / array.length;
+}
+
+
+function quickSort(arr) {
+    if (arr.length < 2) return arr;
+    let pivot = arr[0];
+    const left = [];
+    const right = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (pivot > arr[i]) {
+            left.push(arr[i]);
+        } else {
+            right.push(arr[i]);
+        }
+    }
+    return quickSort(left).concat(pivot, quickSort(right));
 }
